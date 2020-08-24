ba2ea65b592d96f49ef2081b12d2c5563a11e8e6
kigael <kigael16@naver.com>
2020-08-24 21:13:15 +0900

    Initial Push
    this is initial push of jmt-search

---
+++ src/main/java/com/jmtsearch/JmtSearchApplication.java
@@ -0,0 +1,13 @@
+package com.jmtsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmtSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmtSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmtsearch/JmtSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmtsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmtSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
